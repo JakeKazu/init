0baeb168e712ed7a85c83d535aa74cbebdc040a3
kaza9322@gmail.com <kaza9322@gmail.com>
2016-12-25 17:51:26 +0900

    enh-ruby-modeからruby-modeに変更

--- .emacs.d/elpa/ruby-block-20131210.1931/ruby-block-pkg.el
+++ .emacs.d/elpa/ruby-block-20131210.1931/ruby-block-pkg.el
@@ -1,1 +1,1 @@
-(define-package "ruby-block" "20131210.1931" "highlight matching block" (quote nil))
+(define-package "ruby-block" "20131210.1931" "highlight matching block" 'nil :keywords '("languages" "faces" "ruby"))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -225,15 +225,15 @@
 
 ;;;;;;ruby-mode設定;;;;;;
 
-(require 'enh-ruby-mode)
-(autoload 'enh-ruby-mode "enh-ruby-mode"
+(require 'ruby-mode)
+(autoload 'ruby-mode "enh-ruby-mode"
   "Mode for editing ruby source files" t)
-(add-to-list 'auto-mode-alist '("\\.rb" . enh-ruby-mode))  (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb" . ruby-mode))  (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
 ;;do end補完
 (require 'ruby-electric)
-(add-hook 'enh-ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
 (setq ruby-electric-expand-delimiters-list nil)
 
 ;;doやendに対応する行のハイライト
