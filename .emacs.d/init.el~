b05382c3911c375a6b3fea314c9070adcecbc096
kaza9322@gmail.com <kaza9322@gmail.com>
2016-12-25 17:17:21 +0900

    ruby機能追加

--- .emacs.d/init.el~
+++ .emacs.d/init.el~
@@ -82,6 +82,20 @@
 ;;magit -emacsでgitを使う-
 (require 'magit)
 
+;;web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-engines-alist
+'(("php"    . "\\.phtml\\'")
+  ("blade"  . "\\.blade\\.")))
+
 ;;Latex
 ;;yatex
 (require 'yatex)
--- init.el
+++ init.el
@@ -185,26 +185,7 @@
 (add-hook 'java-mode-hook 'flymake-mode-on)
 
 ;;ruby
-;; Invoke ruby with '-c' to get syntax checking
-;(defun flymake-ruby-init ()
-;  (let* ((temp-file   (flymake-init-create-temp-buffer-copy                       'flymake-create-temp-inplace))
-;	 (local-file  (file-relative-name
-;                       temp-file
-;                       (file-name-directory buffer-file-name))))
-;    (list "ruby" (list "-c" local-file))))
-
-;(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-
-;(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-
-;(add-hook 'ruby-mode-hook
-;          '(lambda ()
-
-	     ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
-;	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-;		 (flymake-mode))
-;	     ))
+;rubyの設定で行う
 
 ;;python-python-pyflakes
 ; pipのインストール
@@ -241,6 +222,27 @@
 (add-hook 'post-command-hook 'flymake-show-help)
 
 ;;;;;;文法チェック flymake おわり;;;;;;
+
+;;;;;;ruby-mode設定;;;;;;
+
+(require 'enh-ruby-mode)
+(autoload 'enh-ruby-mode "enh-ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb" . enh-ruby-mode))  (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+
+;;do end補完
+(require 'ruby-electric)
+(add-hook 'enh-ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list nil)
+
+;;doやendに対応する行のハイライト
+;; ruby-block.el --- highlight matching block
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
+
+;;;;;;ruby-mode設定終わり;;;;;;
 
 ;;{とうつと}, "とうつと"のように対応する文字を自動入力
 ;;(electric-pair-mode t)
